6774b11b2a184283e4aeef068c864c91390ea488
mutsuyuki <muttuwo.mob@gmail.com>
2020-04-16 22:11:02 +0900

    add dialog

--- Assets/_UIElementsRuntime/UIElementsHeaderBar/UIElementsHeaderBar.cs
+++ Assets/_UIElementsRuntime/UIElementsHeaderBar/UIElementsHeaderBar.cs
@@ -7,9 +7,20 @@ using UnityEngine.UIElements;
 public class UIElementsHeaderBar : PanelRenderer {
     public void Start() {
         base.Start();
-        var testButton = visualTree.Q<Button>("service-title");
-        if (testButton != null) {
-            testButton.clickable.clicked += () => { Debug.Log("button clicked"); };
+        var logoButton = visualTree.Q<Button>("service-title");
+        if (logoButton != null) {
+            logoButton.clickable.clicked += () => {
+                Debug.Log("logo clicked");
+                EditorUtility.DisplayDialog("logo clicked", "message from header", "ok");
+            };
+        }
+
+        var userNameButton = visualTree.Q<Button>("user-name");
+        if (userNameButton != null) {
+            userNameButton.clickable.clicked += () => {
+                Debug.Log("username clicked");
+                EditorUtility.DisplayDialog("user name clicked", "message from header", "ok");
+            };
         }
     }
 }
